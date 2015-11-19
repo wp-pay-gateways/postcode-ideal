bbad78c92f1bbedc9314ac683ad20c01658c3c7a
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 13:54:09 +0100

    Extends from the iDEAL Advanced v3 library.

--- src/Integration.php
+++ src/Integration.php
@@ -1,21 +1,9 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_PostcodeIDeal_Integration {
+class Pronamic_WP_Pay_Gateways_PostcodeIDeal_Integration extends Pronamic_WP_Pay_Gateways_IDealAdvancedV3_AbstractIntegration {
 	public function __construct() {
 		$this->id       = 'postcode-ideal';
 		$this->name     = 'Postcode iDEAL';
 		$this->provider = 'postcode.nl';
-	}
-
-	public function get_config_factory_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealAdvancedV3_ConfigFactory';
-	}
-
-	public function get_config_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealAdvancedV3_Config';
-	}
-
-	public function get_gateway_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealAdvancedV3_Gateway';
 	}
 }
