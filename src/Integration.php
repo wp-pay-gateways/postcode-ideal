d0f4a132ed665b8962a85d863013d9a05ceed12e
Remco Tolsma <info@remcotolsma.nl>
2016-03-23 10:25:12 +0100

    Getting ready for version 1.0.1.

--- src/Integration.php
+++ src/Integration.php
@@ -7,7 +7,8 @@
  * Company: Pronamic
  *
  * @author Remco Tolsma
- * @version 1.0.0
+ * @version 1.0.1
+ * @since 1.0.0
  */
 class Pronamic_WP_Pay_Gateways_PostcodeIDeal_Integration extends Pronamic_WP_Pay_Gateways_IDealAdvancedV3_AbstractIntegration {
 	public function __construct() {
